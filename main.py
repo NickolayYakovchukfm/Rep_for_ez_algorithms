ccc48f7d3e4063a57afced18a431926bd735ff72
NickolayYakovchukfm <84731499+NickolayYakovchukfm@users.noreply.github.com>
2022-02-06 17:19:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import numpy as np
+class Solution:
+    def isReflected(self, points):
+        if len(points) == 0:
+            return True
+        coordinates_of_symmetry_point = max(points, key=lambda x: x[0])[0] + min(points, key=lambda x: x[0])[0]
+        symmetry_point = coordinates_of_symmetry_point / 2
+        list_of_points = list()
+        list_of_reflected_points = list()
+        for a, b in points:
+            if a > symmetry_point:
+                list_of_points.append((a, b))
+            elif a < symmetry_point:
+                list_of_reflected_points.append((coordinates_of_symmetry_point - a, b))
+        if list_of_reflected_points == list_of_points:
+            return True
+        else:
+            return False
